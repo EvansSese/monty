f0de813311d5d7288fb5558f5cf9c95d39cf0c51
Evans Sese <sese_evans@yahoo.com>
2023-06-22 14:11:15 -0700

    [Feature] Implemented div opcode

--- div.c
+++ div.c
@@ -9,16 +9,16 @@
  */
 void f_div(stack_t **stack, unsigned int line_number)
 {
-        if (*stack == NULL || (*stack)->next == NULL)
-        {
-                fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
-                exit(EXIT_FAILURE);
-        }
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 	if ((*stack)->n == 0)
 	{
 		fprintf(stderr, "L%d: division by zero\n", line_number);
-		exit (EXIT_FAILURE);
+		exit(EXIT_FAILURE);
 	}
-        (*stack)->next->n /= (*stack)->n;
+	(*stack)->next->n /= (*stack)->n;
 	pop(stack, line_number);
 }
