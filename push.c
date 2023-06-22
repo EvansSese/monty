17279ce4e9afcf073744cfe5702ecfeeafd3ad04
Biitopher <biitopher@gmail.com>
2023-06-22 08:55:19 -0700

    Fix[Betty]

--- push.c
+++ push.c
@@ -4,25 +4,24 @@
  * push - Adds element on top of stack
  * @stack: Stack
  * @line_number: line number
- * 
+ *
  * Return: Nothing
  */
 void push(stack_t **stack, unsigned int line_number)
 {
-    stack_t *new_node = malloc(sizeof(stack_t));
+	stack_t *new_node = malloc(sizeof(stack_t));
 
-    if (new_node == NULL)
-    {
-        fprintf(stderr, "Error: malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
 
-    new_node->n = line_number;
-    new_node->prev = NULL;
-    new_node->next = *stack;
+	new_node->n = line_number;
+	new_node->prev = NULL;
+	new_node->next = *stack;
 
-    if (*stack != NULL)
-        (*stack)->prev = new_node;
-
-    *stack = new_node;
+	if (*stack != NULL)
+		(*stack)->prev = new_node;
+	*stack = new_node;
 }
