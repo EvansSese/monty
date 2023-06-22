6276e79f31bc95c37557bd2bf086919bee34c565
Evans Sese <sese_evans@yahoo.com>
2023-06-22 11:33:56 -0700

    [Fix] Git merge fix

--- push.c
+++ push.c
@@ -16,22 +16,11 @@ void push(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
-<<<<<<< HEAD
 	new_node->n = line_number;
 	new_node->prev = NULL;
 	new_node->next = *stack;
 
 	if (*stack != NULL)
 		(*stack)->prev = new_node;
-
-=======
-
-	new_node->n = line_number;
-	new_node->prev = NULL;
-	new_node->next = *stack;
-
-	if (*stack != NULL)
-		(*stack)->prev = new_node;
->>>>>>> dcc1e100b40f06a2304078c52c7a944f9edd0d9a
 	*stack = new_node;
 }
